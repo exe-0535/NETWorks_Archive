a4fe19fe1963f5f85e5b0a60d0cc163e0fda2a5d
exe <igorwdowiczak@gmail.com>
2023-02-25 16:04:15 +0100

    Add project files.

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace NETWorks
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
