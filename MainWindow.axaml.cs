c3ce17177ffa28882f32d526301a2c7a7116f063
exe <igorwdowiczak@gmail.com>
2023-02-25 18:47:06 +0100

    :anchor: Added getIPv4 method, disabled window scaling

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using System.Net;
+using System.Net.NetworkInformation;
 
 namespace NETWorks
 {
@@ -7,6 +9,51 @@ namespace NETWorks
         public MainWindow()
         {
             InitializeComponent();
+
+            networkCard.Content = getNetworkCardName();
+            ipv4.Content = "IPv4: " + getIPv4();
+            ipv6.Content = "IPv6: " + getIPv6();
+            connectionType.Content = getConnectionType();
         }
+
+        public string getIPv4()
+        {
+            string ipv4Address = string.Empty;
+            foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (ni.NetworkInterfaceType == NetworkInterfaceType.Ethernet || ni.NetworkInterfaceType == NetworkInterfaceType.Wireless80211)
+                {
+                    foreach (UnicastIPAddressInformation ip in ni.GetIPProperties().UnicastAddresses)
+                    {
+                        if (ip.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                        {
+                            ipv4Address = ip.Address.ToString();
+                            break;
+                        }
+                    }
+                }
+
+                if (!string.IsNullOrEmpty(ipv4Address))
+                {
+                    break;
+                }
+            }
+            return ipv4Address;
+        }
+
+        public string getIPv6()
+        {
+            return string.Empty;
+        }
+        public string getNetworkCardName()
+        {
+            return string.Empty;
+        }
+
+        public string getConnectionType()
+        {
+            return string.Empty;
+        }
+    
     }
 }
